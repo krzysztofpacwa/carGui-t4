84fce3fdece91293fb419cbfd9c819d34396b718
Christopher Pacwa <krys74@gmail.com>
2020-10-22 00:39:22 +0200

    tydzien4

---
+++ src/main/java/com/pacwa/krys/carapi4/CarApi4Application.java
@@ -0,0 +1,13 @@
+package com.pacwa.krys.carapi4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarApi4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarApi4Application.class, args);
+    }
+
+}
